bbd8bb15a43e44148e32ff95b48aa7c634ee3c31
thanhltde <149461786+Liamm-shkerpear@users.noreply.github.com>
2025-05-26 13:43:00 +0700

    Vector-icons

--- SimpleComponents/ImageIcon/App.tsx
+++ SimpleComponents/ImageIcon/App.tsx
@@ -1,4 +1,5 @@
 import { Image, StyleSheet, View } from 'react-native';
+import {Ionicons} from '@expo/vector-icons';
 
 export default function App() {
   return (
@@ -13,6 +14,10 @@ export default function App() {
       style={{ width: 200, height: 200 }} 
       resizeMode="contain"
       />
+      <Ionicons name='airplane' size={20} color={'red'}/>
+      <Ionicons name='alarm' size={20} color={'blue'}/>
+      <Ionicons name='american-football' size={20} color={'orange'}/>
+      <Ionicons name='battery-charging' size={50} color={'green'}/>
     </View>
   );
 }
