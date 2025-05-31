b2f7e9464c68e43b394abe587c67b1ff14918911
thanhltde <149461786+Liamm-shkerpear@users.noreply.github.com>
2025-05-31 15:55:16 +0700

    Flatlist

--- SimpleComponents/TextInput/App.tsx
+++ SimpleComponents/TextInput/App.tsx
@@ -1,16 +1,23 @@
-import { Alert, Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Alert, Button, StyleSheet, Text, TextInput, View, Image } from 'react-native';
 
 const onPressButton = () => {
-  Alert.alert('Account submitted!')
+  Alert.alert('Login successful!')
 }
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <View style= {{ alignItems: 'center', marginBottom: 20 }}>
+      <Image 
+        source={{ uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/800px-React-icon.svg.png'}}
+        style={{ width: 200, height: 200, justifyContent: 'center' }} 
+        resizeMode="contain"
+      />
+      </View>
       <Text style={{marginBottom: 12}}>UserName:</Text>
       <TextInput
         style={{ height: 40, borderColor: 'black', borderWidth: 1, marginBottom: 16 }}
-        placeholder='Enter your username'
+        placeholder='Ernter your username'
       />
       <Text style={{marginBottom: 12}}>Password:</Text>
       <TextInput
@@ -18,7 +25,8 @@ export default function App() {
         placeholder='Enter your password'
         secureTextEntry={true}
       />
-      <Button title='Submit' onPress={onPressButton}/>
+      <Button title='Login' onPress={onPressButton}/>
+      <Button title='Cancel' onPress={() => Alert.alert('Login cancelled!')} color='red'/>
     </View>
   );
 }
